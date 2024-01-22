73e3f5562fa4f50a85b832ee95c6b849f7a0f82a
Yousuf Mesalm <31203259+Youssefmesalm@users.noreply.github.com>
2024-01-22 20:33:00 +0200

    login  page

---
+++ .history/src/sections/customer/customers-table_20240119202101.js
@@ -0,0 +1,200 @@
+import PropTypes from 'prop-types';
+import { format } from 'date-fns';
+import {
+  Avatar,
+  Box,
+  Card,
+  Checkbox,
+  Collapse,
+  IconButton,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography
+} from '@mui/material';
+import { Scrollbar } from '../../components/scrollbar';
+import { getInitials } from '../../utils/get-initials';
+import {  Visibility } from '@mui/icons-material';
+
+export const CustomersTable = (props) => {
+  const {
+    count = 0,
+    items = [],
+    onDeselectAll,
+    onDeselectOne,
+    onPageChange = () => {},
+    onRowsPerPageChange,
+    onSelectAll,
+    onSelectOne,
+    page = 0,
+    rowsPerPage = 0,
+    selected = []
+  } = props;
+  const [open, setOpen] = React.useState(false);
+  const selectedSome = (selected.length > 0) && (selected.length < items.length);
+  const selectedAll = (items.length > 0) && (selected.length === items.length);
+  const editIcon = (
+    <IconButton>
+      <Visibility color="primary" />
+    </IconButton>
+  );
+  return (
+    <Card>
+      <Scrollbar>
+        <Box sx={{ minWidth: 800 }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell padding="checkbox">
+                  <Checkbox
+                    checked={selectedAll}
+                    indeterminate={selectedSome}
+                    onChange={(event) => {
+                      if (event.target.checked) {
+                        onSelectAll?.();
+                      } else {
+                        onDeselectAll?.();
+                      }
+                    }}
+                  />
+                </TableCell>
+                <TableCell>
+                  Name
+                </TableCell>
+                <TableCell>
+                  Email
+                </TableCell>
+                <TableCell>
+                  Location
+                </TableCell>
+                <TableCell>
+                  Phone
+                </TableCell>
+                <TableCell>
+                  Licsense expiry
+                </TableCell> 
+                <TableCell>
+                  Accounts linked
+                </TableCell>
+                <TableCell>
+                  View As
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {items.map((customer) => {
+                const isSelected = selected.includes(customer.id);
+                const createdAt = format(customer.createdAt, 'dd/MM/yyyy');
+
+                return (
+                  <TableRow
+                    hover
+                    key={customer.id}
+                    selected={isSelected}
+                  >
+                    <TableCell padding="checkbox">
+                      <Checkbox
+                        checked={isSelected}
+                        onChange={(event) => {
+                          if (event.target.checked) {
+                            onSelectOne?.(customer.id);
+                          } else {
+                            onDeselectOne?.(customer.id);
+                          }
+                        }}
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Stack
+                        alignItems="center"
+                        direction="row"
+                        spacing={2}
+                      >
+                        <Avatar src={customer.avatar}>
+                          {getInitials(customer.name)}
+                        </Avatar>
+                        <Typography variant="subtitle2">
+                          {customer.name}
+                        </Typography>
+                      </Stack>
+                    </TableCell>
+                    <TableCell>
+                      {customer.email}
+                    </TableCell>
+                    <TableCell>
+                      {customer.address.city}, {customer.address.state}, {customer.address.country}
+                    </TableCell>
+                    <TableCell>
+                      {customer.phone}
+                    </TableCell>
+                    <TableCell>
+                      {createdAt}
+                    </TableCell>
+                    <TableCell>
+                      {customer.accounts}
+                    </TableCell>
+                    <TableCell>
+                      {editIcon}
+                    </TableCell>
+                   
+                 
+                 
+                   <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                     <Collapse in={open} timeout="auto" unmountOnExit>
+                       <Box sx={{ margin: 1 }}>
+                         <Typography variant="h6" gutterBottom component="div">
+                           History
+                         </Typography>
+                         <Table size="small" aria-label="purchases">
+                           <TableHead>
+                             <TableRow>
+                               <TableCell>Date</TableCell>
+                               <TableCell>Customer</TableCell>
+                               <TableCell align="right">Amount</TableCell>
+                               <TableCell align="right">Total price ($)</TableCell>
+                             </TableRow>
+                           </TableHead>
+                           <TableBody>
+                             
+                           </TableBody>
+                         </Table>
+                       </Box>
+                     </Collapse>
+                   </TableCell>
+                   </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Box>
+      </Scrollbar>
+      <TablePagination
+        component="div"
+        count={count}
+        onPageChange={onPageChange}
+        onRowsPerPageChange={onRowsPerPageChange}
+        page={page}
+        rowsPerPage={rowsPerPage}
+        rowsPerPageOptions={[5, 10, 25]}
+      />
+    </Card>
+  );
+};
+
+CustomersTable.propTypes = {
+  count: PropTypes.number,
+  items: PropTypes.array,
+  onDeselectAll: PropTypes.func,
+  onDeselectOne: PropTypes.func,
+  onPageChange: PropTypes.func,
+  onRowsPerPageChange: PropTypes.func,
+  onSelectAll: PropTypes.func,
+  onSelectOne: PropTypes.func,
+  page: PropTypes.number,
+  rowsPerPage: PropTypes.number,
+  selected: PropTypes.array
+};
